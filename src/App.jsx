6c055700f1601fe7791bff677dadcf1eeafa9fcc
Gustavo Mendes <gustavo568telesmendes@gmail.com>
2025-11-15 21:12:46 -0300

    Implementando as rotas do React

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import Medicos from "./pages/Medicos";
+import Pacientes from "./pages/Pacientes";
+import Atendimentos from "./pages/Atendimentos";
+import Pagamentos from "./pages/Pagamentos";
+import Funcionarios from "./pages/Funcionarios";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      <nav style={{ padding: "10px", background: "#eee" }}>
+        <Link to="/" style={{ marginRight: "10px" }}>
+          Home
+        </Link>
+        <Link to="/Medicos" style={{ marginRight: "10px" }}>
+          Medicos
+        </Link>
+        <Link to="/Pacientes">Pacientes</Link>
+        <Link to="/Atendimentos" style={{ marginLeft: "10px" }}>
+          Atendimentos
+        </Link>
+        <Link to="/Pagamentos" style={{ marginLeft: "10px" }}>
+          Pagamentos
+        </Link>
+        <Link to="/Funcionarios" style={{ marginLeft: "10px" }}>
+          Funcionarios
+        </Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Medicos" element={<Medicos />} />
+        <Route path="/Pacientes" element={<Pacientes />} />
+        <Route path="/Atendimentos" element={<Atendimentos />} />
+        <Route path="/Pagamentos" element={<Pagamentos />} />
+        <Route path="/Funcionarios" element={<Funcionarios />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
