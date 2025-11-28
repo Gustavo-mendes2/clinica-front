9eb92b1b391f777dcfcf693e9bc5a20ee9c5e548
Sp-zip1 <dantassilas3@gmail.com>
2025-11-27 22:28:37 -0300

    mudei tudo, se virem agora!!!

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,28 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link, useLocation } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Medicos from "./pages/medicos/Medicos";
 import Pacientes from "./pages/pacientes/Pacientes";
 import Atendimentos from "./pages/atendimento/Atendimentos";
-import Pagamentos from "./pages/pagamentos/Pagamentos";
 import Funcionarios from "./pages/funcionarios/Funcionarios";
 import "./App.css";
 
-function App() {
+function Layout() {
+  const location = useLocation();
+
+  // transforma a rota em nome de classe
+  const page =
+    location.pathname === "/"
+      ? "home"
+      : location.pathname.replace("/", "").toLowerCase();
+
   return (
-    <Router>
+    <div className={`page-container ${page}`}>
       <nav>
-        <Link to="/" className="navItem">
-          Home
-        </Link>
-        <Link to="/Medicos" className="navItem">
-          Medicos
-        </Link>
-        <Link to="/Pacientes" className="navItem">
-          Pacientes
-        </Link>
-        <Link to="/Atendimentos" className="navItem">
-          Atendimentos
-        </Link>
-        <Link to="/Funcionarios" className="navItem">
-          Funcionarios
-        </Link>
+        <Link to="/" className="navItem">Home</Link>
+        <Link to="/Medicos" className="navItem">Medicos</Link>
+        <Link to="/Pacientes" className="navItem">Pacientes</Link>
+        <Link to="/Atendimentos" className="navItem">Atendimentos</Link>
+        <Link to="/Funcionarios" className="navItem">Funcionarios</Link>
       </nav>
 
       <Routes>
@@ -35,8 +32,14 @@ function App() {
         <Route path="/Atendimentos" element={<Atendimentos />} />
         <Route path="/Funcionarios" element={<Funcionarios />} />
       </Routes>
-    </Router>
+    </div>
   );
 }
 
-export default App;
+export default function App() {
+  return (
+    <Router>
+      <Layout />
+    </Router>
+  );
+}
